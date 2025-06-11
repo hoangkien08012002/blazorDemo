5f6c3b1c4237dc3cc63a03c97f73a2352364e99f
trong <trong@webjapan.co.jp>
2025-06-11 18:03:43 +0700

    update

--- TodoListBlazorWasm/TodoListApi/Controllers/TasksssController.cs
+++ TodoListBlazorWasm/TodoListApi/Controllers/TasksssController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TodoListApi.Entities;
 using TodoListApi.Repositories;
 
 namespace TodoListApi.Controllers
@@ -9,7 +10,8 @@ namespace TodoListApi.Controllers
     public class TasksssController : ControllerBase
     {
         private readonly ITaskssRepository _taskRepository;
-        public TasksssController(ITaskssRepository taskRepository) {
+        public TasksssController(ITaskssRepository taskRepository)
+        {
             _taskRepository = taskRepository;
         }
         [HttpGet]
@@ -17,6 +19,52 @@ namespace TodoListApi.Controllers
         {
             var list = await _taskRepository.GetTaskssList();
             return Ok(list);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Taskss task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var createTask = await _taskRepository.Create(task);
+            return CreatedAtAction(nameof(GetById), createTask);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> update(Guid id, Taskss task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var taskOld = await _taskRepository.GetTaskssById(id);
+
+            if (taskOld == null)
+            {
+                return NotFound($"{id} not found");
+            }
+
+
+            var ta = await _taskRepository.Create(task);
+            return Ok(ta);
+        }
+
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid Id)
+        {
+
+            var getById = await _taskRepository.GetTaskssById(Id);
+            if (getById == null)
+            {
+                return NotFound($"{Id} not found");
+            }
+            return Ok(getById);
         }
     }
 }
