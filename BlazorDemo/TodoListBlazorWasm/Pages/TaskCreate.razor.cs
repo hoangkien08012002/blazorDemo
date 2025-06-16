d699e9ea5b3cd150855d62e081e55bfd5804a932
trong <hoangkien08012002@gmail.com>
2025-06-16 17:20:14 +0700

    toast( draft)

--- BlazorDemo/TodoListBlazorWasm/Pages/TaskCreate.razor.cs
+++ BlazorDemo/TodoListBlazorWasm/Pages/TaskCreate.razor.cs
@@ -5,12 +5,14 @@ using TodoListModel.Enums;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components;
 using TodoListBlazorWasm.Services;
+using Blazored.Toast.Services;
 namespace TodoListBlazorWasm.Pages
 {
     public partial class TaskCreate
     {
         [Inject] private ITaskApiClient taskApi { get; set; }
         [Inject] NavigationManager navigationManager { get; set; }
+        [Inject] IToastService toastService { get; set; }
         private TaskCreateRequest task = new TaskCreateRequest();
 
         private async Task CreateTask(EditContext editContext)
@@ -18,11 +20,12 @@ namespace TodoListBlazorWasm.Pages
             var create = await taskApi.Create(task);
             if (create)
             {
+                toastService.ShowSuccess($"{task.Name} has been created.");
                 navigationManager.NavigateTo("taskList");
             }
             else
             {
-
+                toastService.ShowError("An error occurred in progress");
             }
         }
     }
